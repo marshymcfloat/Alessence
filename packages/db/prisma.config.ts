a7d74ba9138e5f0d788b61df2bc5a5cbafb7fa0b
Daniel Canoy <canoydaniel06@gmail.com>
2025-11-16 01:48:05 +0800

    modifying prisma config 2

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,5 +1,13 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+import fs from "fs";
+import dotenv from "dotenv";
+
+if (fs.existsSync(".env")) {
+  dotenv.config({ path: ".env" });
+} else {
+  dotenv.config({ path: ".env.example" });
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
