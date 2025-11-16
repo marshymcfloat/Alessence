ad1726787a026b05c985ac3ea46a44f7aacb5f62
Daniel Canoy <canoydaniel06@gmail.com>
2025-11-16 23:16:37 +0800

    making it more general

--- apps/web/components/landing/SignInDialog.tsx
+++ apps/web/components/landing/SignInDialog.tsx
@@ -24,7 +24,7 @@ const SignInDialog = () => {
               Alessence
             </DialogTitle>
             <DialogDescription className="text-center">
-              Goodluck, baby! galingan mo!
+              Goodluck!
             </DialogDescription>
           </DialogHeader>
 
