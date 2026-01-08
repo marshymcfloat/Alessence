064746368129f320647209dd644878914bf94c63
Daniel Canoy <canoydaniel06@gmail.com>
2026-01-08 14:42:33 +0800

    fix: transpile ESM packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/db", "@prisma/client", "nuqs"],
+  transpilePackages: [
+    "@repo/db",
+    "@prisma/client",
+    "nuqs",
+    "react-markdown",
+    "remark-gfm",
+  ],
   images: {
     unoptimized: false,
     remotePatterns: [],
